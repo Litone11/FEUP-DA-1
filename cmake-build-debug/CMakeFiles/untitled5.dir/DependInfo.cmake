25344787f9dcadb5cb4e5982a97ffd23e9e678b2
Luís Martins <luis.a110686@gmail.com>
2025-06-22 18:34:58 +0100

    Imported

---
+++ cmake-build-debug/CMakeFiles/untitled5.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/luismartins/CLionProjects/untitled5/batch.cpp" "CMakeFiles/untitled5.dir/batch.cpp.o" "gcc" "CMakeFiles/untitled5.dir/batch.cpp.o.d"
+  "/Users/luismartins/CLionProjects/untitled5/graph.cpp" "CMakeFiles/untitled5.dir/graph.cpp.o" "gcc" "CMakeFiles/untitled5.dir/graph.cpp.o.d"
+  "/Users/luismartins/CLionProjects/untitled5/main.cpp" "CMakeFiles/untitled5.dir/main.cpp.o" "gcc" "CMakeFiles/untitled5.dir/main.cpp.o.d"
+  "/Users/luismartins/CLionProjects/untitled5/parser.cpp" "CMakeFiles/untitled5.dir/parser.cpp.o" "gcc" "CMakeFiles/untitled5.dir/parser.cpp.o.d"
+  "/Users/luismartins/CLionProjects/untitled5/route.cpp" "CMakeFiles/untitled5.dir/route.cpp.o" "gcc" "CMakeFiles/untitled5.dir/route.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
